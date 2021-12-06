51fab8223a559f8b4606413419bfe9f48e81799b
BruPrestes <brunnoprestes@gmail.com>
2021-12-06 15:21:30 -0300

    Obtendo usuario por id

--- src/main/java/com/bruprestes/workshopmongo/services/UserService.java
+++ src/main/java/com/bruprestes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.bruprestes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bruprestes.workshopmongo.domain.User;
 import com.bruprestes.workshopmongo.repository.UserRepository;
+import com.bruprestes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,11 @@ public class UserService {
 		return repo.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
+
+
