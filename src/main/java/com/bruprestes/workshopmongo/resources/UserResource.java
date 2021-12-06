f6f5563dca48e844343d69f59936d33b8d2bb587
BruPrestes <brunnoprestes@gmail.com>
2021-12-06 19:13:16 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/bruprestes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/bruprestes/workshopmongo/resources/UserResource.java
@@ -51,7 +51,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
-
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 	
