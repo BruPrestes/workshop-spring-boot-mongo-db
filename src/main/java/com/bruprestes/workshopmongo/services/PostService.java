af70a48f164408401a49273f9f14355fe833a1ff
BruPrestes <brunnoprestes@gmail.com>
2021-12-13 17:41:33 -0300

    Consulta com vários critérios

--- src/main/java/com/bruprestes/workshopmongo/services/PostService.java
+++ src/main/java/com/bruprestes/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.bruprestes.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,19 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	}
 
