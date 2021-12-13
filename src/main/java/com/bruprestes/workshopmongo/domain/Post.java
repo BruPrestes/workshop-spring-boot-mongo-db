284b8bb68b36a8d14ad1ee9e4d7d003f79fc7b52
BruPrestes <brunnoprestes@gmail.com>
2021-12-13 00:58:20 -0300

    Acrescentando comentário ao post

--- src/main/java/com/bruprestes/workshopmongo/domain/Post.java
+++ src/main/java/com/bruprestes/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.bruprestes.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.bruprestes.workshopmongo.dto.AuthorDTO;
+import com.bruprestes.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+	 
 	public Post() {
 	}
 
@@ -91,6 +96,18 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 	
 	
